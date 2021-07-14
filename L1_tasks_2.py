f640a18c49f6f7b04f85bf5efa57c792dda8ea60
Roman <mamchiy@yandex.ru>
2021-07-15 01:46:49 +0300

    Tasks for logical bitwise operations

---
+++ L1_tasks_2.py
@@ -0,0 +1,20 @@
+# Task 2
+
+def bitwiseOperations():
+    binMessege = 'а в бинарном представлении будет '
+    shiftMessege = 'Битовый сдвиг числа 5 на два знака '
+    bitwiseNumbers = (5, 6)
+    shiftRight = bitwiseNumbers[0] >> 2
+    shiftLeft = bitwiseNumbers[0] << 2
+    print(f'{shiftMessege} вправо будет {shiftRight}, {binMessege} {bin(shiftRight)}; \n{shiftMessege} влево '
+          f'будет {shiftLeft}, {binMessege}{bin(shiftLeft)}')
+    print('Побитовые операторы предназначены для работы с данными в битовом (двоичном) формате.'
+          'Побитовый сдвиг, значение операнда "сдвигается" влево или вправо на количество бит указанных в операнде.')
+    bitAnd = bitwiseNumbers[0] & bitwiseNumbers[1]
+    print(f'Логические побитовые операции «И», над {bitwiseNumbers[0]} и {bitwiseNumbers[1]}, будет  {bitAnd};'
+          f' при операции «ИЛИ», над {bitwiseNumbers[0]} и {bitwiseNumbers[1]}, будет  {bitAnd};')
+    print('Логический оператор "И". Условие будет истинным если оба операнда истина.\n'
+          'Логический оператор "ИЛИ". Если хотя бы один из операндов истинный, то и все выражение будет истинным.')
+
+
+bitwiseOperations()
