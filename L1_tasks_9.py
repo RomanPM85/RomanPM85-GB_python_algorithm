00d1314761b8b8972e9d8155e10dcd59aa84b225
Roman <mamchiy@yandex.ru>
2021-07-17 12:23:32 +0300

    adjustment of task 9

--- L1_tasks_9.py
+++ L1_tasks_9.py
@@ -1,11 +1,22 @@
-print('Введите три числа: ')
-a = int(input())
-b = int(input())
-c = int(input())
+# Task 9
 
-if b < a < c or c < a < b:
-    print('Среднее:', a)
-elif a < b < c or c < b < a:
-    print('Среднее:', b)
-else:
-    print('Среднее:', c)
+def average_number():
+    """
+     Вводятся три разных числа. Найти, какое из них является средним (больше одного, но меньше другого).
+    :return:
+    """
+    print('Введите три числа: ')
+    a = int(input('Первое число: '))
+    b = int(input('Второе число: '))
+    c = int(input('Третье число: '))
+    messages = "Среднее число: "
+
+    if b < a < c or c < a < b:
+        print(f'{messages}{a}')
+    elif a < b < c or c < b < a:
+        print(f'{messages}{b}')
+    else:
+        print(f'{messages}{c}')
+
+
+average_number()
